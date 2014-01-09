4045806d54b7a2d07f680bf997e2ed8f4294969f
3751_Creator <3751_Creator>
2014-01-09 21:06:07 +0100

    Bonjours.

--- src/fr/Gouda/FTP/Frame.java
+++ src/fr/Gouda/FTP/Frame.java
@@ -18,7 +18,7 @@ import javax.swing.JTree;
 
 public class Frame extends JFrame implements ActionListener {
 
-	private static final long serialVersionUID = 1L; // Salut je vais faire un peu plus.
+	private static final long serialVersionUID = 1L; // Salut je vais faire un peu plus. Kikou lol.
 
 		public JButton	JButton_Parcourir_Local = new JButton("Parcourir"),
 						JButton_Parcourir_FTP = new JButton("Parcourir");
