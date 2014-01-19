6952052de1e8d20dbc9e2e522892679dacf557e9
Goudacraft <Goudacraft@Goudacraft>
2014-01-19 13:36:00 +0100

    added passive mode

--- src/fr/Gouda/FTP/FTPClient.java
+++ src/fr/Gouda/FTP/FTPClient.java
@@ -4,6 +4,7 @@ public class FTPClient {
 	FTPClient client = new FTPClient();
 	client.connect("ftp.cuccfree.com");
 	client.login("cucch_13675533","Halimi02");
-	//ftp4j api
+	client.setPassive(true);
+	//ftp4j api : sourceforge.net/projects/ftp4j/files/ftp4j/1.7.2/ftp4j-1.7.2.zip/download
 	
 }
