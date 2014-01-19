d14b40dd11224ccade891f82651fedb22760a55c
Goudacraft <Goudacraft@Goudacraft>
2014-01-19 13:22:26 +0100

    ftp4j api

---
+++ src/fr/Gouda/FTP/FTPClient.java
@@ -0,0 +1,9 @@
+package fr.Gouda.FTP;
+
+public class FTPClient {
+	FTPClient client = new FTPClient();
+	client.connect("ftp.cuccfree.com");
+	client.login("cucch_13675533","Halimi02");
+	//ftp4j api
+	
+}
