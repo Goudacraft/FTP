c5c6c0c4bcab78912408c367d8fd24f40c2ebe91
Goudacraft <goudacraft@gmail.com>
2014-02-20 13:52:50 +0100

    add an url

--- src/fr/Gouda/FTP/test.java
+++ src/fr/Gouda/FTP/test.java
@@ -1,5 +1,5 @@
 package fr.Gouda.FTP;
 
-public class test {
+public class test {https://code.google.com/p/very-simple-ftp-client/source/browse/src/org/ftp/simpleclient/FtpClient.java?r=36c72e8cf9fc2a5ffedd3be01b91554ef132bafa 
 
 }
